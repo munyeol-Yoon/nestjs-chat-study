8eab8c206171bf467e65d8eec8cdd6e26b6c33a8
Mun-Yeol Yoon <gmakin36@gmail.com>
2023-12-31 23:48:19 +0900

    socket setup

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,18 @@
-alert('안녕 세상아');
+const socket = io('/');
+
+const getElementById = (id) => document.getElementById(id) || null;
+
+//  get DOM element
+const helloStrangerElement = getElementById('hello_stranger');
+const chattingBoxElement = getElementById('chatting_box');
+const formElement = getElementById('chat_form');
+
+function helloUser() {
+  const username = prompt('이름이 뭐에욥?');
+}
+
+function init() {
+  helloUser();
+}
+
+init();
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ChatsGateway } from './chats/chats.gateway';
+import { ChatsModule } from './chats/chats.module';
 import mongoose from 'mongoose';
 
 @Module({
@@ -10,9 +12,10 @@ import mongoose from 'mongoose';
       isGlobal: true,
     }), // isGlobal 옵션으로 다른 모듈에서 Config 옵션을 가져올 필요가 없어진다.
     MongooseModule.forRoot(process.env.MONGO_URI),
+    ChatsModule,
   ],
   controllers: [AppController],
-  providers: [],
+  providers: [ChatsGateway],
 })
 export class AppModule implements NestModule {
   configure() {
